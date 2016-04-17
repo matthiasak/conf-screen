0e40e01a83f444941bc0f6d65ab3ff7c3d12b56d
Matthew Keas <matthiasak@gmail.com>
2016-04-17 15:30:42 -0500

    making updates

--- js/server.js
+++ js/server.js
@@ -4,14 +4,15 @@ process.on('exit', function() {
 })
 
 const cluster = require('cluster')
-import sticky from 'socketio-sticky-session'
 import pem from 'pem'
 import {app} from './koa'
 const config = require('../config.json')
 import spdy from 'spdy'
 const http = require('http')
-import socketIo from 'socket.io'
+// import socketIo from 'socket.io'
+// import sticky from 'socketio-sticky-session'
 const os = require('os')
+// import uws from 'uws'
 
 var port = process.argv[3] || process.env.PORT || 3000 //(process.getuid() === 0) ? 443 : 3000
 
@@ -28,10 +29,20 @@ keys.then(({serviceKey:key, certificate:cert}) => {
         getServer = () => {
             if(config.https){
                 const server = spdy.createServer(credentials, app.callback())
-                const io = socketIo.listen(server)
-                io.on('connection', function(socket) {
+                // socketio
+                // const io = socketIo.listen(server)
+                // io.on('connection', function(socket) {
                     // TODO: do stuff with socket
-                })
+                // })
+                // uws
+                // var UWSServer = uws.Server
+                // var wss = new UWSServer({ port: 8080 })
+                // wss.on('connection', ws => {
+                //     ws.on('message', message => {
+                //         console.log('received: ' + message)
+                //     })
+                //     ws.send('something')
+                // })
                 return server
             }
 
@@ -39,13 +50,13 @@ keys.then(({serviceKey:key, certificate:cert}) => {
         }
 
     if (config.cluster) {
-        sticky({
-            // https://github.com/wzrdtales/socket-io-sticky-session
-            num: os.cpus(), // process count
-            proxy: false, // if the layer 4 patching should be used or not, needed if behind a proxy.
-        }, getServer).listen(port, function() {
-            console.log('Cluster worker ' + (cluster.worker ? cluster.worker.id : '') + ' server listening on port ' + port)
-        })
+        // sticky({
+        //     // https://github.com/wzrdtales/socket-io-sticky-session
+        //     num: os.cpus(), // process count
+        //     proxy: false, // if the layer 4 patching should be used or not, needed if behind a proxy.
+        // }, getServer).listen(port, function() {
+        //     console.log('Cluster worker ' + (cluster.worker ? cluster.worker.id : '') + ' server listening on port ' + port)
+        // })
     } else {
         getServer().listen(port, function() {
             console.log('server (no cluster) listening on port ' + port)
